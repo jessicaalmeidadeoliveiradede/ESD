93982496ecf99e2ff62caa3dde88d4f5fd8e6fce
Jéssica Almeida de Oliveira <32886156+jessicaalmeidadeoliveiradede@users.noreply.github.com>
2024-03-20 18:49:10 -0300

    Add files via upload

---
+++ aula3.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct departamento{
+    int cod;
+    char descricao;
+} departamento;
+typedef struct cargo{
+    int cad;
+    char descricao;
+}cargo;
+typedef struct funcionario{
+    int cad;
+    char nome;
+    float salario;
+     Departamento depto;
+     cargo cargo;
+}funcionario;
+ printf( "\n TEXTO %_" , nome_vetor_struct[indice].nome_membro_struct);
