c73160468a7cdd6111818c97d2eb87ef202b71f2
Jéssica Almeida de Oliveira <32886156+jessicaalmeidadeoliveiradede@users.noreply.github.com>
2024-03-20 18:50:50 -0300

    Add files via upload

---
+++ main.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+struct aluno{
+    int v_nmat;
+    float v_nota [3];
+    float v_media;
+};
+{
+    strcct aluno felipe;
+    felipe.v_nmat=120;
+    falipe.v_nota[0]=8,5;
+    falipe.v_nota[1]=7,2;
+    felipe.v_nota[2]=5,4;
+    felipe.v_madia=( felipe.v_notas[0]+felipe.v_nota[1]+felipe.v_nota[2])/3.0;
+
+    printf("matricula:%d\n",felipe.v_nmat);
+    printf("media:%2.f\n",felipe.v_media);
+    system("pause");
+    return 0;
+}
